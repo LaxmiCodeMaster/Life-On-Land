e014131dc41e0c8cecfb1c0ee9aecd6a7c65b9ea
LaxmiCodeMaster <ly3929366@gmail.com>
2026-06-03 08:51:16 +0545

    elephannt photo

--- src/data/mock-data.ts
+++ src/data/mock-data.ts
@@ -33,7 +33,7 @@ export const animals: Animal[] = [
     name: "Asian Elephant",
     scientificName: "Elephas maximus",
     image:
-      "https://www.fauna-flora.org/species/asian-elephant/",
+      "https://plus.unsplash.com/premium_photo-1710021012562-b3a94af19725?q=80&w=1243&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     status: "Endangered",
     dangerLevel: "danger",
     population: 47000,
